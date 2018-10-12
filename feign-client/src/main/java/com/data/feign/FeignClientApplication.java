da2a9aa9a7b5b3a0b083d72da1dcbf714de4f31f
chuanxingzhang <chuanxingzhang@126.com>
2018-10-12 15:43:26 +0800

    hystrix dashboard

--- feign-client/src/main/java/com/data/feign/FeignClientApplication.java
+++ feign-client/src/main/java/com/data/feign/FeignClientApplication.java
@@ -4,14 +4,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableDiscoveryClient
+
+@EnableCircuitBreaker
+@EnableHystrixDashboard
+
+
 public class FeignClientApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FeignClientApplication.class, args);
     }
+
 }
