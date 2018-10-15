a80a24704b2cf75ef1da296d4f85b332d86766df
chuanxingzhang <chuanxingzhang@126.com>
2018-10-15 11:40:14 +0800

    zuul 过滤器

---
+++ zuul/src/main/java/com/data/zuul/config/ZuulFilterConfig.java
@@ -0,0 +1,39 @@
+package com.data.zuul.config;
+
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+
+@Component
+public class ZuulFilterConfig extends ZuulFilter {
+    private static Logger log = LoggerFactory.getLogger(ZuulFilterConfig.class);
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        String s = String.format("%s >>> %s", request.getMethod(), request.getRequestURL().toString());
+        log.info(s);
+        return null;
+    }
+}
